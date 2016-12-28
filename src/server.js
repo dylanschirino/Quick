e1d23e8b9b1d93707d2be4de1a7cb6785de67242
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-12-28 12:42:01 +0100

    Import default server file

---
+++ src/server.js
@@ -0,0 +1,17 @@
+/* Dylan/Kash
+ *
+ * /gulpfile - Server.js
+ *
+ * coded by Dylan Schirino
+ * started at 28/12/2016
+ */
+
+// init db
+import { init as initDB } from "./core/mongodb.js";
+import { init as initExpress } from "./core/express";
+const APP_PORT = 12345;
+// init expresssss
+initDB()
+  .then( () => {
+    initExpress( APP_PORT );
+  } );
