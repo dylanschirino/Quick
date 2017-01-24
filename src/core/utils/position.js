dac928c705ed668e3601049bfda2a5a6cb6ec5c3
Dylan Schirino <dylan.schirino@hotmail.fr>
2017-01-24 15:37:56 +0100

    Remove useless variable oPosition

--- src/core/utils/position.js
+++ src/core/utils/position.js
@@ -7,17 +7,16 @@
  */
  export default function( iLatitude, iLongitude ) {
 
-     let oPosition;
-     
+
      // Si ce n'est pas un chiffre on s'arrete
      if ( isNaN( iLatitude ) || isNaN( iLongitude ) ) {
          return false;
      }
-     // On verifie qu'on bien sur la surface de la terre
+     // On verifie qu'on bien sur la surface de la terre au niveau de la latitude
      if ( iLatitude < -90 || iLatitude > 90 ) {
          return false;
      }
-     // On verifie qu'on bien sur la surface de la terre
+     // On verifie qu'on bien sur la surface de la terre au niveau de la longitude
      if ( iLongitude < -180 || iLongitude > 180 ) {
          return false;
      }
